ab99414e905fd1979086ab2a0cd4b74b6acbdaca
vishalk-singh <charumitrasingh@gmail.com>
2025-05-20 19:14:12 +0530

    Finished ArchwayKarel.py.

--- ArchwayKarel.py
+++ ArchwayKarel.py
@@ -12,7 +12,48 @@ so Karel ends up on the right side of it facing East.
 4. Karel should end up facing East.
 """
 def main():
-    pass
+    # Turn till north is facing and move till wall is in front of Karel.
+    # Turn till east is facing and move till wall is in front of Karel.
+    # Turn till south is facing and move till wall is in front of Karel and at the bottom of the archway.
+    # Turn till east is facing and move till wall is in front of Karel.
+    climb_archway()
+    move_to_wall()
+    descend_archway()
+# Turn till north is facing
+# Move till wall is in front of Karel
+# Turn till east is facing
+def climb_archway():
+    face_north()
+    move_to_wall()
+    face_east()
+
+# Turn till south is facing
+# Move till wall is in front of Karel
+# Turn till east is facing
+def descend_archway():
+    face_south()
+    move_to_wall()
+    face_east()
+
+# Turn till south is facing
+def face_south():
+    while not facing_south():
+        turn_left()
+
+# Move till wall is in front of Karel
+def move_to_wall():
+    while front_is_clear():
+        move()
+
+# turn till east is facing
+def face_east():
+    while not facing_east():
+        turn_left()
+
+# turn till north is facing
+def face_north():
+    while not facing_north():
+        turn_left()
 
 
 
